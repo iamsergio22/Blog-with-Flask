853fb22bbc6556f2acd7e0a2a6b47bb1ebb4fb3c
sergio <isergioinf@gmail.com>
2022-08-11 17:55:56 -0500

    50% de conexion a bd

--- hello.py
+++ hello.py
@@ -1,16 +1,45 @@
-from flask import flash, Flask, render_template
+from flask import flash, Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_mysqldb import MySQL
+import mysql.connector
+
 
 # Crear una instancia de Flask
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "mi super clave secrssseta"
 
 # crear una clase form
+
+
 class NamerForm(FlaskForm):
     name = StringField("What's your name?", validators=[DataRequired()])
     submit = SubmitField("Submit")
+
+
+# conexion a db
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    db='blog',
+)
+
+# INSERTAR EN BD
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        email = request.form['email']
+        cur = conn.connection.cursor()
+        cur.execute(
+            "INSERT INTO users (nombre,correo) VALUES (%s,%s)", (nombre, email))
+        mysql.connection.commit()
+        print("Registro exitoso")
+
 
 # Crear una ruta a un decorador
 @app.route('/')
@@ -23,7 +52,7 @@ def user(name):
     return render_template("user.html", name=name)
 
 
-@app.route('/name', methods=['GET','POST'])
+@app.route('/name', methods=['GET', 'POST'])
 def name():
     name = None
     form = NamerForm()
@@ -33,9 +62,6 @@ def name():
         form.name.data = ''
         flash("Fom submiteed succesfully")
     return render_template("name.html", name=name, form=form)
-
-
-
 
 
 # ------------------------------------------------------------
