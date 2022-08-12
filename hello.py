ff231b81bddfe038f1089a09b79d552f58c61993
sergio <isergioinf@gmail.com>
2022-08-12 11:54:12 -0500

    inserta y consulta en la bd

--- hello.py
+++ hello.py
@@ -1,22 +1,22 @@
-from flask import flash, Flask, render_template, request
+from flask import flash, Flask, render_template, request,url_for,redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-from flask_mysqldb import MySQL
-import mysql.connector
+from flask_mysqldb import MySQL #Necesario para la conexion a DB
+import mysql.connector #Necesario para la conexion a DB
 
 
 # Crear una instancia de Flask
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "mi super clave secrssseta"
 
-# crear una clase form
+# Ventana por defecto/index
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 
-class NamerForm(FlaskForm):
-    name = StringField("What's your name?", validators=[DataRequired()])
-    submit = SubmitField("Submit")
-
+# BASE DE DATOS ⬇️⬇️⬇️⬇️⬇️⬇️
 
 # conexion a db
 conn = mysql.connector.connect(
@@ -27,24 +27,30 @@ conn = mysql.connector.connect(
 )
 
 # INSERTAR EN BD
-
-
 @app.route('/add', methods=['POST'])
 def add():
-    if request.method == 'POST':
-        nombre = request.form['nombre']
+    if request.method == 'POST': #Si a esta ruta se le envia algo atraves del metodo 'POST' entonces haga esto:
+        nombre = request.form['nombre'] #la variable nombre va a ser igual a lo que haya en el name="nombre" del name.html 
         email = request.form['email']
-        cur = conn.connection.cursor()
+        fecha=request.form['fecha']
+        cur = conn.cursor()
         cur.execute(
-            "INSERT INTO users (nombre,correo) VALUES (%s,%s)", (nombre, email))
-        mysql.connection.commit()
-        print("Registro exitoso")
+            'INSERT INTO users (nombre,correo,Fecha) VALUES (%s,%s,%s)', (nombre, email,fecha))
+        conn.commit()        
+        flash("User Registrado!!!")
+        return redirect(url_for('name')) # Estoy redireccionando a la funcion name que tiene la vista name y tambien me consulta lo que hay en la base de datos.
+
+# SEARCH TO DB 
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM users')
+    data = cur.fetchall()
+    conn.commit()       
+    return render_template('name.html', users=data)
 
 
-# Crear una ruta a un decorador
-@app.route('/')
-def index():
-    return render_template("index.html")
+
 
 
 @app.route('/user/<name>')
@@ -52,16 +58,7 @@ def user(name):
     return render_template("user.html", name=name)
 
 
-@app.route('/name', methods=['GET', 'POST'])
-def name():
-    name = None
-    form = NamerForm()
-    # validaciones
-    if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-        flash("Fom submiteed succesfully")
-    return render_template("name.html", name=name, form=form)
+
 
 
 # ------------------------------------------------------------
